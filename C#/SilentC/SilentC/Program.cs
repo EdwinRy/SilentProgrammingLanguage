f96d887b36cc7d1c8fc24c2cb37f83382f986678
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2017-03-11 14:02:48 +0000

    initialise

---
+++ C#/SilentC/SilentC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilentC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Pass an argument of the path to the main file to be compiled");
+        }
+
+    }
+}
