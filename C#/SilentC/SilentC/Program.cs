d16c0b1fe6bf15149947c3148453cf8edf33a3fa
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2017-03-17 22:35:16 +0000

    k

--- C#/SilentC/SilentC/Program.cs
+++ C#/SilentC/SilentC/Program.cs
@@ -10,12 +10,29 @@ namespace SilentC
     {
         static void Main(string[] args)
         {
+            if(args.Contains("-c"))
+            {
+                string CompileFilePath;
+            }
         }
 
         static void Main()
         {
+            Console.WriteLine("-c compiles the file and returns the bytecode file of the same name as the input file");
+            Console.WriteLine("-o set the output name of the generated file");
             Console.WriteLine("Pass an argument of the path to the main file to be compiled");
         }
 
+        static void Compile(string filePath)
+        {
+
+        }
+
+        /*
+        static void Compile(string )
+        {
+
+        }
+        */
     }
 }
--- C/src/Silent.c
+++ C/src/Silent.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main()
 
+int main(int argc, char *argv[] ){
+    //if argc == 2 there's only 1 argument passed
     return 0;
+    
 }
