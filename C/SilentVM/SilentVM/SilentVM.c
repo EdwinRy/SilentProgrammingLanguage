bcdfbb7f5e7f2c3f75f46a0acac843296e6aab57
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2017-05-09 22:34:43 +0100

    .

---
+++ C/SilentVM/SilentVM/SilentVM.c
@@ -0,0 +1,29 @@
+#include "SilentVM.h"
+
+void CreateSilentVM(SilentVM * vm, SilentStack * stack, int * ScriptLength)
+{
+}
+
+void DeleteSilentVM(SilentVM * vm)
+{
+}
+
+void CreateSilentStack(SilentStack * stack)
+{
+}
+
+void CreateSilentStack(SilentStack * stack, int stackSize)
+{
+}
+
+void DeleteSilentStack(SilentStack * stack)
+{
+}
+
+void UpdateStackSize(SilentStack * stack, int newStackSize)
+{
+}
+
+void UpdateStackSize(SilentStack * stack)
+{
+}
--- C/SilentVM/SilentVM/SilentVM.h
+++ C/SilentVM/SilentVM/SilentVM.h
@@ -1,6 +1,33 @@
 #pragma once
+#ifndef SILENTVM
+#define SILENTVM
 
-struct SilentVM {
 
-};
+typedef struct SilentStack {
+
+    char* memory;
+    char* storage;
+
+}SilentStack;
+
+
+typedef struct SilentVM {
+
+    SilentStack stack;
+
+}SilentVM;
+
+
+void CreateSilentVM(SilentVM *vm, SilentStack *stack, int *ScriptLength);
+void DeleteSilentVM(SilentVM *vm);
+
+void CreateSilentStack(SilentStack *stack, int stackSize);
+void DeleteSilentStack(SilentStack *stack);
+
+void UpdateStackSize(SilentStack *stack, int newStackSize);
+
+
+#endif // !SilentVM
+
+
 
