ad9a5f81e7689e637c1ddd8b13a7694320d725dc
Edwin Rybarczyk <edwinrybarczyk4d@gmail.com>
2017-08-09 20:12:36 +0100

    lexer additional functions

--- Silent/SilentCompiler/Lexer.c
+++ Silent/SilentCompiler/Lexer.c
@@ -9,3 +9,18 @@ void Tokenize(char* source, char* tokens)
 
 }
 
+char isEqualString(char* string1, char* string2) 
+{
+	int len1 = strlen(string1);
+	int len2 = strlen(string2);
+	if (len1 == len2) 
+	{
+		for(int i = 0; i <= len1; i++)
+		{
+			if (string1[i] != string2[i]) {return 0;}
+		}
+		return 1;
+	}
+	else {return 0;}
+}
+
