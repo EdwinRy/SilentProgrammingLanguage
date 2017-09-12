5fa290c4da4b05ddb3b969c1b239db628f88c26e
Edwin Rybarczyk <edwinrybarczyk4d@gmail.com>
2017-09-12 21:25:12 +0100

    assembler update

--- Silent/SilentCompiler/Compiler.c
+++ Silent/SilentCompiler/Compiler.c
@@ -12,16 +12,30 @@ void Compile(char * source)
 	//printf("%c",*(program->source+1));
 	Tokenize(program);
 
-	for (int i = 0; i <= program->tokensPointer; i++) {
+	for (int i = 0; i <= program->tokensPointer; i++) 
+	{
 		printf("%i \n", *(program->tokens + 1));
 		getchar();
 	}
 }
 
+char** split(char* string, long long stringLength) 
+{
+	char** tokens = malloc(stringLength);
+	long long numberOfTokens = 0;
+	char* buffer = malloc(256);
+	int bufferPointer = 0;
+
+	//count tokens
+	for (int i = 0; i < stringLength; i++) {
+		if (string[i] == ' ' || string[i] == '\n')
+			numberOfTokens++;
+	}
+}
 
 void CompileAssembly(assembly* asm)
 {
-
+	char** tokens = split(asm->code, asm->codeSize);
 	printf(asm->outputFile);
 
 }
