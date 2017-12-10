6f7e2cca83f899248eaf3b947f6827c4701558fb
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2017-12-10 22:13:22 +0000

    ok

--- SilentASM/src/SilentASM.c
+++ SilentASM/src/SilentASM.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
+typedef enum silentType
+{
+    
+}silentType;
+typedef struct silentInstruction
+{
+    char* data;
+
+}silentType;
 char assemble(char* inFile, char* outFile)
 {
     FILE* f;
@@ -17,7 +26,32 @@ char assemble(char* inFile, char* outFile)
 
     while(size != -1)
     {
-        
+        char buffer[255];
+        char* instructions[2];
+        for(int i = 0; i < size+1;i++)
+        {
+            if(isalpha(line[i]))
+            {
+                //Number of characters
+			    unsigned char count = 0;
+			    //prepare space for the value
+			    char* value;
+			    //As long as the current character isn't whitespace
+			    while(isalpha(line[i+count]) ||
+				    isdigit(line[i+count]))
+			    {
+				    //Assign buffer to the current character
+				    buffer[count++] = line[i+count];
+			    }
+			    //Allocate space for the value and terminator
+			    value = malloc(count+1);
+			    //Copy the value from the buffer
+			    memcpy(value,buffer,count);
+			    value[count] = '\0';
+			    token.value = value;
+			    token.type = silentIdentifierToken;
+            }
+        }
     }
 
     return 1;
@@ -27,5 +61,5 @@ int main()
 {
     char* in = "";
     char* out = "";
-
+    assemble(in, out);
 }
