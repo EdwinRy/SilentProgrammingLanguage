08b7cddbc5a1e56e3ebb9c6215698cdaca3fd326
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2018-10-17 23:44:46 +0100

    ok

--- SilentCompiler/src/SilentCleanup.cpp
+++ SilentCompiler/src/SilentCleanup.cpp
@@ -2,7 +2,7 @@
 using namespace Silent;
 typedef unsigned long long uint64;
 
-#define DEBUG 1
+#define DEBUG 0
 
 void freeVariable(SilentVariable* variable)
 {
--- SilentCompiler/src/SilentCodeGen.cpp
+++ SilentCompiler/src/SilentCodeGen.cpp
@@ -10,18 +10,29 @@ typedef unsigned int uint32;
 
 #define DEBUG 1
 
-
-
 struct CompilerData
 {
     std::string currentToken;
-    std::string currentNamespace;
+    //std::string currentNamespace;
     std::string currentFunction;
-    std::vector<std::string> scope;
+    std::vector<SilentNamespace*> currentNamespace;
     std::unordered_map<std::string, uint64> symTable;
     uint64 cp;
 };
 CompilerData cd;
+
+std::string genScopeName(std::string id)
+{
+    std::string name = "";
+
+    for(SilentNamespace* scope : cd.currentNamespace)
+    {
+        name += scope->name; name += ":";
+    }
+
+    name += id;
+    return name;
+}
 
 std::string compileExpression(SilentOperand& expression)
 {
@@ -43,6 +54,8 @@ std::string compileFunction(SilentFunction& func)
         std::cout << "Compiling function: " << func.name << "\n";
     #endif
 
+    std::cout << "Scope name: " << genScopeName(func.name) << "\n";
+
     #if DEBUG
         std::cout << "Done compiling function: " << func.name << "\n";
     #endif
@@ -51,12 +64,23 @@ std::string compileFunction(SilentFunction& func)
 
 std::string compileNamespace(SilentNamespace& scope)
 {
-    std::string output;
+    std::string output = "";
     #if DEBUG
         std::cout << "Compiling namespace: " << scope.name << "\n";
     #endif
 
+    cd.currentNamespace.push_back(&scope);
 
+    //Compile nested namespace
+    for(SilentNamespace* nScope : scope.namespaces)
+        output += compileNamespace(*nScope);
+
+    //Compile namespace functions
+    for(SilentFunction* func : scope.functions)
+        output += compileFunction(*func);
+
+
+    cd.currentNamespace.pop_back();
 
     #if DEBUG
         std::cout << "Done compiling namespace: " << scope.name << "\n";
@@ -67,7 +91,7 @@ std::string compileNamespace(SilentNamespace& scope)
 std::string Silent::SilentCompileAST(SilentParserInfo& info)
 {
     cd.cp = 0;
-    std::string output;
+    std::string output = "";
     output += compileNamespace(*info.globalNamespace);
     return output;
 }
